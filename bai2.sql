4175401fb5d0ecb201e41b4afd680a2587d88ad3
ngoclee_280907 <ngoclee280907@gmail.com>
2026-04-16 22:54:17 +0700

    Add files via upload

--- bai2.sql
+++ bai2.sql
@@ -1,18 +1,16 @@
-set SQL_SAFE_UPDATES = 0;
-drop database if exists Quanlysieuthi;
-create database Quanlysieuthi;
-use Quanlysieuthi;
+create database btap_sieuthi;
+use btap_sieuthi;
 
 create table shippers (
-    ShipperID int primary key auto_increment ,
-    ShipperName varchar(255),
-    Phone varchar(20)
+    id int primary key auto_increment,
+    ten_dv varchar(100),
+    sdt varchar(15)
 );
 
-insert into shippers (ShipperName, Phone)
-values ('Giao hàng nhanh', '0901234567');
+insert into shippers (ten_dv, sdt) 
+values ('Giao hang nhanh', '0901234567');
 
-insert into shippers (ShipperName, Phone)
+insert into shippers (ten_dv, sdt) 
 values ('Viettle Post', '0123456789');
 
 select * from shippers;
